12f9f7b64438a2c864dc8bc379fad3444498197d
Parker McCrary <parkertomm3@gmail.com>
2020-10-15 21:08:30 -0400

    comment update

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
+// Require dependencies
 const express = require('express');
 
 const app = express();
 
+// Adds static assets in public folder
 app.use('/static', express.static('public'));
 
+// sets view engine to pug files
 app.set('view engine', 'pug');
 
+
+// Middleware
 const mainRoute = require('./routes');
 
 app.use(mainRoute);
 
+// 404 Error
 app.use((req, res, next) => {
     const err = new Error(`This page doesn't exist`);
     err.status = 404;
     next(err);
 });
 
+// Depending on error show that type of error
 app.use((err, req, res, next) => {
+    // If error status is 404, load page-not-found page
+    // If error status is 500, load error page
     if(err.status === 404) {
         res.locals.error = err;
         res.status = (err.status);
@@ -28,6 +37,7 @@ app.use((err, req, res, next) => {
     }
 });
 
+// localhost setup
 app.listen(3000, () => {
     console.log('/******* Starting App on localhost:3000 *******/')
 })
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
+// Require dependencies
 const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
+// Landing page route
 router.get('/', (req, res) => {
     res.render('index', { projects });
 });
 
+// About page route
 router.get('/about', (req, res) => {
     res.render('about');
 });
 
+// project pages routes
 router.get('/projects/:id', (req, res, next) => {
     const project = projects.find((project) => project.id.toString() === req.params.id);
     
+    // If a project exists, load that page
+    // Otherwise display a custom 404 Error page
     if(project) {
         res.render('project', { project });
     } else {
